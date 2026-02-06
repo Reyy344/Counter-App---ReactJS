830a2f7be9e3ce81cb5c46cad3859994ab065250
Reyy344 <141038093+Reyy344@users.noreply.github.com>
2026-02-06 13:34:45 +0700

    Update react.js

--- react.js
+++ react.js
@@ -1,1 +1,32 @@
+      const container = document.getElementById("root");
+      const root = ReactDOM.createRoot(container);
 
+      function HomePage() {
+        const [number, setNumber] = React.useState(0);
+
+        function handleClickPlus() {
+          setNumber(number + 1);
+        }
+
+        function handleClickMinus() {
+          setNumber(number - 1);
+        }
+
+        function resetHandle() {
+          setNumber(0);
+        }
+
+        return (
+          <>
+            <h3>Counter App - ReactJS</h3>
+            <div>
+              <button onClick={handleClickMinus}>-</button>
+              <span>({number})</span>
+              <button onClick={handleClickPlus}>+</button>
+            </div>
+
+            <button onClick={resetHandle}>Reset</button>
+          </>
+        );
+      }
+      root.render(<HomePage />);
